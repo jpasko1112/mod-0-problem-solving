64316e3c0640a7b65c911ed9083a372508248219
Justin Pasko <jpasko1112@gmail.com>
2023-02-14 13:34:00 -0500

    Add pseudocode to challenge2.rb

--- callenge2.rb
+++
@@ -1,9 +0,0 @@
-# 1. Start with an array of strings with a mix of uppercase and lowercase letters. Print every word in all lowercase letters.
-
-examples = ["File", "tRust", "sheEp", "chEese"]
-
-    examples.each do |example|
-        puts example.downcase!
-    end
-
-puts examples
--- challenge2.rb
+++ challenge2.rb
@@ -1,5 +1,11 @@
 # 1. Start with an array of strings with a mix of uppercase and lowercase letters. Print every word in all lowercase letters.
 
+# End goal is to have a string that started with upper & lower case letters and have it returned all lowercase.
+#Data types are Arrays and Strings.
+# What method/s will i need to use?
+# First, create an array with multiple strings that have upper and lowercase letters. Next, run a looping method to check each string inside the array. Next, use a method what will down case any characters that are upcased. Finally, eturn the results.
+
+
 examples = ["File", "tRust", "sheEp", "chEese"]
 
     examples.each do |example|
