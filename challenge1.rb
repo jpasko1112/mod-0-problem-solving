47e64952c1af122a053d2a4789300885db055d3d
Justin Pasko <jpasko1112@gmail.com>
2023-02-14 13:26:28 -0500

    Add pseudocode to challenge1.rb

--- challenge1.rb
+++ challenge1.rb
@@ -1,5 +1,10 @@
 # 1. Given an array of strings, return only the strings that have exactly 4 characters.
 
+# Write code to return strings with only 4 characters.
+# Working with Array & Strings.
+# What method/s will i need to complete this.
+# First, creat an Array with strings that have characters >= 4. Insert a method to check each string. Insert method that checks each string for 4 characters. Return strings that are ONLY 4 characters.
+
 words = ["file", "strange", "flower", "jazz", "nelson"]
 
     words.each do |word|
