a156f2c456aa838a675aa4fbe4369c7b6e00932f
Justin Pasko <jpasko1112@gmail.com>
2023-02-14 13:36:58 -0500

    Add pseudocode for challenge3.rb

--- challenge3.rb
+++ challenge3.rb
@@ -1,5 +1,9 @@
 # 1. Given an array of strings, return only the words that begin with the letter "t".
 
+# End goal is to return strings that begin with "t" from an Array.
+# Data used is Array & Strings
+# First, create an Array with strings that begin with "t" and other letters. Next, use a looping method to check each string. Next, use a method that will check each strings first character for "t". Finally, return the results.
+
 names = ["tyler", "tim", "walter", "nemo", "horris"]
 
     names.each do |name|
